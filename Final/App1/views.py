c1011528e2e0a8bd4b4bc7b82f375b665a390fb0
ChuchooFerraro <guidoaferraro@gmail.com>
2022-05-19 21:17:37 -0300

    Actualice para colocar plantilla de boostrap y reenderizado simple desde return

--- Final/App1/views.py
+++ Final/App1/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader, Template,Context
+from datetime import datetime
+
 
 # Create your views here.
+
+def fechaa(request):
+    dia= datetime.now()
+    texto = f"Hoy es: <br>{dia}"
+    return HttpResponse(texto)
+
+def azul(request):
+    return render(request, "App1/azul.html")
+
+def start_boostrap(request):
+    return render(request, "App1/index.html")
