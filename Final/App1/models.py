f537421f6083628d050106051a3a4514a896308a
ChuchooFerraro <guidoaferraro@gmail.com>
2022-05-25 21:36:21 -0300

    agregue 2 clases: profesor y alumno

--- Final/App1/models.py
+++ Final/App1/models.py
@@ -7,11 +7,11 @@ class Curso(models.Model):
     camada = models.IntegerField()
 
 class Alumno(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     born = models.DateField()
     email = models.EmailField()
 
 class Profesor(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     born = models.DateField()
     email = models.EmailField()
