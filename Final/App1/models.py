f2842faee72ba454ab440e0570b91f4ed3a3628a
ChuchooFerraro <guidoaferraro@gmail.com>
2022-05-25 21:35:27 -0300

    agregue 2 clases: profesor y alumno

--- Final/App1/models.py
+++ Final/App1/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class Curso(models.Model):
     curso = models.CharField(max_length=20)
     camada = models.IntegerField()
+
+class Alumno(models.Model):
+    name = models.CharField()
+    born = models.DateField()
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    name = models.CharField()
+    born = models.DateField()
+    email = models.EmailField()
