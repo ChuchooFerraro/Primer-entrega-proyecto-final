86498a2c49f7583b5a3787f34759a6c66a7099f2
ivandimas <105029123+ivandimas@users.noreply.github.com>
2022-05-31 14:08:01 -0500

    Delete Final directory

--- Final/App1/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from App1.models import Curso
-
-# Register your models here.
-admin.site.register(Curso)
--- Final/App1/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class Cursoformulario(forms.Form):
-    curso = forms.CharField()
-    camada = forms.IntegerField()
